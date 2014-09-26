e0da8a0001cb555d61f81c71c915785d9836cecb
Vinicius Souza <hi@vsouza.com>
2014-09-26 11:00:09 -0300

    convert HS 2000 and 2001 to 1996x2002

--- exportacao/prepare/transform.py
+++ exportacao/prepare/transform.py
@@ -1,7 +1,4 @@
 from helpers import read_from_csv
-from helpers import find_in_df
-import pandas as pd
-import pandas.io.parsers as parser
 
 ''' Transform '''
 def transform(year):
@@ -40,22 +37,50 @@ def transform(year):
     df = read_from_csv(source_file, 1, None, cols, None)
 
     if year == 2000 or year == 2001:
-
+        ## 1996x2002
         rdCols = ['HS96', 'HS02']
-        rd = read_from_csv('docs/classificacao/HS/HS_96_2002.csv', 1, ';', rdCols, None)
+        rd = read_from_csv('docs/classificacao/HS/anos/1996x2002.csv', 1, ';', rdCols, converters={"HS96": str})
 
         f = lambda x: rd['HS02'][rd.HS96 == str(x)]
         df.apply(f)
+
+        # CONVERT TO 2001
+
+        # CONVERT TO 2007
+
+
         print df
 
     elif year > 2001 and year <= 2006:
+        ##
+        rdCols = ['HS02', 'HS07']
+        rd = read_from_csv('docs/classificacao/HS/HS_2002_2007.csv', 1, ';', rdCols, converters={"HS07": str})
+
+        f = lambda x: rd['HS02'][rd.HS07 == str(x)]
+        df.apply(f)
+
         print df
 
-    elif year > 2007 and year <= 2011:
-        print df
-    elif year > 2012 and year <= 2014:
+    elif year >= 2007 and year <= 2011:
+
+        rdCols = ['HS2012', 'HS2007']
+        rd = read_from_csv('docs/classificacao/HS/HS_2007X2012.csv', 1, ';', rdCols, converters={"HS2007": str})
+
+        f = lambda x: rd['HS2012'][rd.HS2007 == str(x)]
+        df.apply(f)
+
         print df
 
+    elif year >= 2012 and year <= 2014:
+
+        rdCols = ['HS2012', 'HS2007']
+        ## PROCURAR ARQUIVO
+        rd = read_from_csv('docs/classificacao/HS/HS_2007X2012.csv', 1, ';', rdCols, converters={"HS2007": str})
+
+        f = lambda x: rd['HS2012'][rd.HS2007 == str(x)]
+        df.apply(f)
+
+        print df
 
 
 if __name__ == "__main__":
