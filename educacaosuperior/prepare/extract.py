bd1535020773b40b665a9fefedec8dbba6173985
Vinicius Souza <hi@vsouza.com>
2014-10-01 16:59:01 -0300

    extract educacaosuperior 2010

--- educacaosuperior/prepare/extract.py
+++ educacaosuperior/prepare/extract.py
@@ -30,7 +30,27 @@ def extract(year):
 
         print dfCurso
 
+    elif year == 2010:
 
+        # ALUNOS
+
+        colsAlunos = ('CO_IES', 'CO_CATEGORIA_ADMINISTRATIVA', 'CO_ORGANIZACAO_ACADEMICA', 'CO_CURSO', 'NO_CURSO', 'CO_ALUNO_CURSO', 'CO_ALUNO', 'CO_ALUNO_SITUACAO', 'CO_GRAU_ACADEMICO', 'CO_MODALIDADE_ENSINO', 'CO_NIVEL_ACADEMICO', 'IN_MATRICULA', 'IN_CONCLUINTE', 'IN_INGRESSO', 'ANO_INGRESSO', 'DT_INGRESSO_CURSO', 'IN_SEXO_ALUNO', 'NU_IDADE_ALUNO', 'CO_COR_RACA_ALUNO')
+
+        dfAlunos = read_from_csv(source_file, 1,"|", colsAlunos, None)
+
+        df_to_csv(dfAlunos, export_file, None)
+
+        print dfAlunos
+
+        # CURSO
+
+        colsCurso = ('CO_IES', 'CO_CATEGORIA_ADMINISTRATIVA', 'CO_ORGANIZACAO_ACADEMICA', 'CO_NIVEL_ACADEMICO', 'CO_MODALIDADE_ENSINO', 'CO_GRAU_ACADEMICO', 'CO_CURSO', 'NO_CURSO', 'CO_MUNICIPIO', 'CO_OCDE_AREA_GERAL', 'NO_AREA_GERAL', 'CO_OCDE_AREA_ESPECIFICA', 'NO_AREA_ESPECIFICA', 'CO_OCDE_AREA_DETALHADA', 'NO_AREA_DETALHADA', 'CO_OCDE', 'NO_OCDE', 'IN_MATUTINO_CURSO', 'IN_VESPERTINO_CURSO', 'IN_NOTURNO_CURSO', 'IN_INTEGRAL_CURSO', 'QT_MATRICULA_CURSO', 'QT_CONCLUINTE_CURSO', 'QT_VAGAS_INTEGRAL_PRES', 'QT_VAGAS_MATUTINO_PRES', 'QT_VAGAS_NOTURNO_PRES', 'QT_VAGAS_VESPERTINO_PRES', 'QT_VAGAS_ANUAL_EAD', 'QT_VAGAS_CURSO')
+
+        dfCurso = read_from_csv(source_file, 1,"|", colsCurso, None)
+
+        df_to_csv(dfCurso, export_file, None)
+
+        print dfCurso
 
 
 if __name__ == '__main__':
