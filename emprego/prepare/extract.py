0f3e311407e79ffe4ed4693abd5207e0cf043404
Vinicius Souza <hi@vsouza.com>
2014-11-25 16:03:52 -0200

    Some modules #25

--- emprego/prepare/extract.py
+++ emprego/prepare/extract.py
@@ -2,9 +2,7 @@ from helpers import *
 import re
 import os
 
-COLS_2002 =  ('CAUSA DESLI', 'CAUSA DESLI_Fonte', 'OCUPACAO', 'OCUPACAO_desc', 'IND CEI VINC', 'CLAS CNAE 95', 'CLAS CNAE 95_Fonte', 'GRAU INSTR', 'GRAU INSTR_Fonte', 'HORAS CONTR', 'IDADE', 'IDENTIFICAD', 'IND PAT', 'IND PAT_Fonte', 'IND SIMPLES', 'IND SIMPLES_Fonte', 'MUNICIPIO', 'MUNICIPIO_Fonte', 'NACIONALIDAD', 'NACIONALIDAD_Fonte', 'NATUR JUR', 'NATUR JUR_Fonte', 'PIS', 'RACA_COR OR', 'RACA_COR OR_Fonte', 'RADIC CNPJ', 'REM DEZ (R$)', 'REM DEZEMBRO', 'REM MED (R$)', 'SUBS IBGE', 'SEXO', 'SEXO_Fonte', 'TAMESTAB', 'TAMESTAB_Fonte', 'TEMP EMPR', 'TIPO ADM', 'TIPO ADM_Fonte', 'TIPO ESTBL', 'TIPO ESTBL_Fonte', 'TP VINCL', 'DT NASCIMENT', 'MES DESLIG', 'MES DESLIG_Fonte', 'DT ADMISSAO' )
 
-COLS_2002_SEPARATOR = "|"
 
 COLS_2003 = ('CAUSA DESLIG', 'CAUSA DESLIG_Fonte', 'CBO 2002', 'CB0 2002_fonte', 'CEI VINC', 'CLAS CNAE 95', 'CLAS CNAE 95_Fonte', 'EMP  EM 31/12', 'EMP EM 31/12_Fonte', 'GRAU INSTR', 'GRAU INSTR_Fonte', 'HORAS CONTR', 'IDADE', 'IDENTIFICAD', 'IND CEI VINC', 'IND CEI VINC_Fonte', 'IND PAT', 'IND PAT_Fonte', 'IND SIMPLES', 'IND SIMPLES_Fonte', 'MUNICIPIO', 'MUNICIPIO_Fonte', 'NACIONALIDAD', 'NACIONALIDAD_Fonte', 'NATUR JUR', 'NATUR JUR_Fonte', 'PIS', 'RACA_COR', 'RACA_COR_Fonte', 'RADIC CNPJ', 'REM DEZ (R$)', 'REM DEZEMBRO', 'REM MED (R$)', 'REM MEDIA', 'SUBS IBGE', 'SEXO', 'SEXO_Fonte', 'TAMESTAB', 'TAMESTAB_Fonte', 'TEMP EMPR', 'TIPO ADM', 'TIPO ADM_Fonte', 'TIPO ESTBL', 'TIPO ESTBL_Fonte', 'TP VINCULO', 'TP VINCULO_Fonte', 'DT NASCIMENT', 'DT_ADMISSAO', 'MES DESLIG', 'MES DESLIG_Fonte')
 
@@ -57,103 +55,98 @@ COLS_2013 = ('Município', 'CNAE 95 Classe', 'Vínculo Ativo 31/12', 'Tipo Vincu
 
 COLS_2013_SEPARATOR = ";"
 
+""" Open file to CBO conversion"""
+def openCBO94x2002():
+    return read_from_csv('docs/classificacao/CBO/Conversion_CBO94_CBO2002.csv', 1, ';', ['CBO94', 'CBO2002'])
+
+
+
+
+""" Open File for CNAE conversion"""
+def openCNAE10x20():
+    return read_from_csv('docs/classificacao/CNAE/CNAE20_Correspondencia10x20.csv', 1, ';', ['CNAE 10', 'CNAE 20'], converters={'CNAE 20':int, 'CNAE 10':int})
+
+
+
+
 """ Extract CSV file by year """
 def extract(year):
 
-
-    """ Convert CBO094 to CBO2002 """
     if year == 2002:
 
-        file_rais = 'RAIS2002TOTAL_CORTE'
-
-        source_file = 'dados/rais/raw/' + str(year) + '/' + file_rais + '.txt'
-        export_file = 'dados/rais/sent/' + str(year) + '_extractCORTE.csv'
 
 
+        source_file = 'dados/rais/raw/' + str(year) + '/' + 'RAIS2002TOTAL_CORTE' + '.txt'
 
-        """CBO CONVERSION"""
-        cbo_file = 'docs/classificacao/CBO/Conversion_CBO94_CBO2002.csv'
+        cols =  ('CAUSA DESLI', 'CAUSA DESLI_Fonte', 'OCUPACAO', 'OCUPACAO_desc', 'IND CEI VINC', 'CLAS CNAE 95', 'CLAS CNAE 95_Fonte', 'GRAU INSTR', 'GRAU INSTR_Fonte', 'HORAS CONTR', 'IDADE', 'IDENTIFICAD', 'IND PAT', 'IND PAT_Fonte', 'IND SIMPLES', 'IND SIMPLES_Fonte', 'MUNICIPIO', 'MUNICIPIO_Fonte', 'NACIONALIDAD', 'NACIONALIDAD_Fonte', 'NATUR JUR', 'NATUR JUR_Fonte', 'PIS', 'RACA_COR OR', 'RACA_COR OR_Fonte', 'RADIC CNPJ', 'REM DEZ (R$)', 'REM DEZEMBRO', 'REM MED (R$)', 'SUBS IBGE', 'SEXO', 'SEXO_Fonte', 'TAMESTAB', 'TAMESTAB_Fonte', 'TEMP EMPR', 'TIPO ADM', 'TIPO ADM_Fonte', 'TIPO ESTBL', 'TIPO ESTBL_Fonte', 'TP VINCL', 'DT NASCIMENT', 'MES DESLIG', 'MES DESLIG_Fonte', 'DT ADMISSAO' )
 
-        cols_cbo = ['CBO94', 'CBO2002']
+        useCols = ('OCUPACAO', 'CLAS CNAE 95_Fonte', 'GRAU INSTR_Fonte', 'IDADE', 'IDENTIFICAD', 'IND SIMPLES_Fonte', 'MUNICIPIO_Fonte', 'PIS', 'RACA_COR OR_Fonte', 'REM DEZ (R$)', 'REM MED (R$)', 'SEXO_Fonte', 'TAMESTAB_Fonte')
 
-        cbo = read_from_csv(cbo_file, 1, ';', cols_cbo)
+        separator = "|"
 
-        """MapIgnorado"""
+        df = read_from_csv(source_file, 1, separator, cols, None, useCols)
+
+
+        """ Convert CBO094 to CBO2002 """
+        cbo = openCBO94x2002()
+
+        # MapIgnorado
         cbo['CBO94'] [cbo['CBO94'] == "IGNORADO"]= '-1'
+        cbo['CBO94'] = cbo.apply(lambda f: to_int(f['CBO94']) , axis = 1)
+        cbo['CBO2002'] = cbo.apply(lambda f: to_int(f['CBO2002']) , axis = 1)
 
 
-        def convert(row):
-            return int(row['CBO94'])
+        """ Make CBO Conversion """
+        def CBOConversion(row):
+            converted = cbo['CBO2002'][cbo['CBO94'] == row['OCUPACAO']]
 
-        def convert2(row):
-            return int(row['CBO2002'])
-
-        cbo['CB0094'] = cbo.apply(convert, axis=1)
-        cbo['CBO2002'] = cbo.apply(convert2, axis=1)
-
-        def cboConversion(row):
-            converted = cbo['CBO2002'][cbo['CB0094'] == row['OCUPACAO']]
             if len(converted) > 1:
-                converted = converted.head(1)
+                return int(converted.head(1))
 
-            return to_int(converted)
+            return int(converted)
+
+        # MapRetiraTextosLixo
+        df['OCUPACAO'] [df['OCUPACAO'] == "CLAS CNAE"]= ''
+        df['OCUPACAO'] [df['OCUPACAO'] == "CLASSE"]= ''
+        df['OCUPACAO'] [df['OCUPACAO'] == "CLASS"]= ''
+        df['OCUPACAO'] [df['OCUPACAO'] == "CLAS"]= ''
+        df['OCUPACAO'] [df['OCUPACAO'] == "CBO"]= ''
+        df['OCUPACAO'] [df['OCUPACAO'] == "0000-1"]= -1
+
+        """ remove string from field OCUPACAO (CBO)"""
+        df['OCUPACAO'] = df.apply(lambda f : re.findall('\d+', f['OCUPACAO'])[0], axis = 1)
+
+        df['OCUPACAO'] = df.apply(lambda f : to_number(f['OCUPACAO']), axis = 1)
+
+        df['OCUPACAO'] = df.apply(CBOConversion, axis=1)
 
         """CNAE CONVERSION"""
+        cnae = openCNAE10x20()
 
-        cnae_file = 'docs/classificacao/CNAE/CNAE20_Correspondencia10x20.csv'
-
-        cols_cnae = ['CNAE 10', 'CNAE 20']
-
-        cnae = read_from_csv(cnae_file, 1, ';', cols_cnae)
-
+        """ Make CNAE Conversion """
         def cnaeConversion(row):
             converted = cnae['CNAE 20'][cnae['CNAE 10'] == row['CLAS CNAE 95_Fonte']]
-            if len(converted) > 1:
-                converted = converted.head(1)
+            if len(converted) > 0:
+                return int(converted.head(1))
+            return int(converted)
 
-            return to_int(converted)
+        df['CLAS CNAE 95_Fonte'] = df.apply(cnaeConversion, axis=1)
 
-        def cnaeConversion(row):
-            converted = cnae['CNAE 20'][cnae['CNAE 10'] == row['CLAS CNAE 95_Fonte']]
-            if len(converted) > 1:
-                converted = converted.head(1)
+        df['SEXO_Fonte'] = df.apply(lambda x : str(x['SEXO_Fonte']), axis=1)
 
-            return to_int(converted)
-
-        useCols = ('ID',  'OCUPACAO', 'CLAS CNAE 95_Fonte', 'GRAU INSTR_Fonte', 'IDADE', 'IDENTIFICAD', 'IND SIMPLES_Fonte', 'MUNICIPIO_Fonte', 'PIS', 'RACA_COR OR_Fonte', 'REM DEZ (R$)', 'REM MED (R$)', 'SEXO_Fonte', 'TAMESTAB_Fonte')
-
-        df = read_from_csv(source_file, 2,"|", COLS_2002, None, useCols)
-
-        def toString(row):
-            return str(row['SEXO_Fonte'])
-
-        df['SEXO_Fonte'] = df.apply(toString, axis=1)
-
-        """MapGenero"""
+        # MapGenero
         df['SEXO_Fonte'] [df['SEXO_Fonte'] == "MASCULINO"]= 1
         df['SEXO_Fonte'] [df['SEXO_Fonte'] == "FEMININO"]= 0
         df['SEXO_Fonte'] [df['SEXO_Fonte'] == "2" ]= 0
         df['SEXO_Fonte'] [df['SEXO_Fonte'] == "02" ]= 0
         df['SEXO_Fonte'] [df['SEXO_Fonte'] == "01" ]= 1
 
-        """ remove string from field ocupacao"""
-        def toIntCBO(row):
-            cbo_digit = re.findall('\d+', row['OCUPACAO'])
-            return str(cbo_digit[0])
-
-        df['OCUPACAO'] [df['OCUPACAO'] == "0000-1"]= '-1'
-        df['OCUPACAO'] = df.apply(toIntCBO, axis=1)
-
-        df['OCUPACAO'] = df.apply(lambda f : to_number(f['OCUPACAO']) , axis = 1)
-
-        df['OCUPACAO'] = df.apply(cboConversion, axis=1)
-
-        df['CLAS CNAE 95_Fonte'] = df.apply(cnaeConversion, axis=1)
+        export_file = 'dados/rais/sent/' + str(year) + '_extractCORTE.csv'
 
         df_to_csv(df, export_file, None)
 
     elif year > 2002 and year < 2005:
-    """ Maps: CNAE, MapGenero """
+        """ Maps: CNAE, MapGenero """
 
         folder = "dados/rais/raw/" + str(year) + '/'
 
@@ -455,6 +448,6 @@ def extract(year):
 
 if __name__ == '__main__':
     start = time.time()
-    extract(2003)
+    extract(2002)
     total_run_time = time.time() - start
     print "Total runtime: " + format_runtime(total_run_time)
