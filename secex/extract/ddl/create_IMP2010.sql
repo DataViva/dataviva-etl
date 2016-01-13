39d81a7e842303dbe7dc9eec36dd0da34f5ad7a9
Victor Moraes <vdmoraes94@gmail.com>
2016-01-13 11:27:38 -0200

    Criação dos scripts de criação de tabelas SECEX 2008 a 2013

---
+++ secex/extract/ddl/create_IMP2010.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SECEX_2010_IMP (
+	CO_ANO numeric(4),
+	CO_MES char(2),
+	CO_NCM_POS char(4),
+	CO_PAIS char(3),			
+	CO_UF char(2),
+	CO_PORTO char(4),		
+	CO_MUN_GEO char(7),
+	KG_LIQUIDO numeric(7),
+	VL_FOB	numeric(10)
+);
