bf12f46e7e1141259487b6cab3853ddf1d17873e
Victor Moraes <vdmoraes94@gmail.com>
2016-01-13 12:10:13 -0200

    Criação dos scripts de criação de tabelas SECEX 2003 a 2007

---
+++ secex/extract/ddl/create_EXP2005.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SECEX_2005_EXP (
+	CO_ANO numeric(4),
+	CO_MES char(2),
+	CO_SH4 char(4),
+	CO_PAIS char(3),			
+	CO_UF char(2),
+	CO_PORTO char(4),		
+	CO_MUN_GEO char(7),
+	KG_LIQUIDO numeric(7),
+	VL_FOB	numeric(10)
+);
