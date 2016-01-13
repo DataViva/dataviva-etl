751f290497cd74c34f9e0866be2c19590f959c05
Victor Moraes <vdmoraes94@gmail.com>
2016-01-13 12:42:36 -0200

    Criação dos scripts de load do banco SECEX 2000 a 2002

---
+++ secex/extract/load/extract_IMP2002.py
@@ -0,0 +1,29 @@
+import os, sys, time, click, bz2, MySQLdb, magic, codecs, re
+import pandas
+from write_data import write_data
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+''' USAGE EXAMPLE:
+python extract_IMP2002.py data/IMP_2002_MUN.csv '''
+
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True), required=True)
+
+def main(input_file):
+    
+    # Discover encoding type of file
+    with open(input_file, 'r') as fp:
+        first_line = fp.readline()
+    m = magic.Magic(mime_encoding=True)
+    encode = m.from_buffer(first_line)
+
+    header = ['CO_ANO', 'CO_MES', 'CO_SH4', 'CO_PAIS', 'CO_UF', 'CO_PORTO', 'CO_MUN_GEO', 'KG_LIQUIDO', 'VL_FOB']
+    data = pandas.read_csv(input_file, sep=';',names = header, dtype = {'CO_MES' : object, 'CO_SH4': object, 'CO_PAIS': object,
+    'CO_UF': object,
+    'CO_PORTO': object,
+    'CO_MUN_GEO': object}, encoding = encode, nrows = 1000 )
+
+    write_data('SECEX_2002_IMP', 100, data)
+
+main()
