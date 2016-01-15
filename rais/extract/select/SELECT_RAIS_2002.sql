47927574157ea95fd76f7ee014d6c508e163d052
Gilmar de Alcantara <gilmardealcantara@gmail.com>
2016-01-15 09:58:47 -0200

    select rais 2002

--- rais/extract/select/SELECT_RAIS_2002.sql
+++ rais/extract/select/SELECT_RAIS_2002.sql
@@ -1,25 +1,24 @@
 
 
-show tables;
-#select  * from raise limit 5 offset 10;
 
 /*
+	criar tabelas RAIS2002TOTAL e RAIS2002TOTALe com script rais/extract/ddl/create_RAIS_2002.sql
+*/
+#select  * from RAIS2002TOTAL limit 5 offset 1
+drop procedure rowperrow;
+
 DELIMITER $$
 CREATE PROCEDURE ROWPERROW()
 BEGIN
     DECLARE n INT DEFAULT 0;
     DECLARE i INT DEFAULT 0;
-    SELECT COUNT(*) FROM raise INTO n;
+    SELECT COUNT(*) FROM RAIS2002TOTAL INTO n;
     SET i=0;
     WHILE i<n DO 
-        SELECT * FROM raise LIMIT i,5;
-        SET i = i + 5;
+        INSERT INTO RAIS2002TOTAL2 SELECT * FROM RAIS2002TOTAL LIMIT i, 60000;
+        SET i = i + 60000;
     END WHILE;
 END $$
 DELIMITER ;
 
-
-
-call rowperrow();
-
-*/
+#call rowperrow();
