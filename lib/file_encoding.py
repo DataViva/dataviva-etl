a4b008d38c8d95b5089851ba93550703addd6883
FredericoVieira <fredops1@hotmail.com>
2016-01-16 13:23:37 -0200

    Adding a function to discover file enconding in more cases.

--- lib/file_encoding.py
+++ lib/file_encoding.py
@@ -1,7 +1,15 @@
 import magic
-# Discover encoding type of file reading fist line
+
+#Discover encoding type of file reading fist line
 def file_encoding(file_path):
     with open(file_path, 'r') as fp:
         first_line = fp.readline()
     m = magic.Magic(mime_encoding=True)
     return m.from_buffer(first_line)
+
+    #Some files need to be fully read to discover right encoding.
+    #In this cases use the following code:
+    #Discover encoding type of file
+    #fp = open(file_path).read()
+    #m = magic.Magic(mime_encoding=True)
+    #return m.from_buffer(fp)
