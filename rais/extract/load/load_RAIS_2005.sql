91c85f86e4c6bebdcb146907edc7f67fa6ef5860
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-01-25 12:49:48 -0200

    Carregando RAIS 2005

---
+++ rais/extract/load/load_RAIS_2005.sql
@@ -0,0 +1,12 @@
+load data local infile 'H:/RAIS/Dados/2005/RAISCOMPL_SE_31122005.txt'
+into table RAIS_2005
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2005/RAISCOMPL_NO_NE_CO_SU_31122005.txt'
+into table RAIS_2005
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
