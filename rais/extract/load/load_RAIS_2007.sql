4009817a4568ebdcb34ff4d10cb623a6c106033a
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-01-27 12:33:07 -0200

    Carregando RAIS 2007

---
+++ rais/extract/load/load_RAIS_2007.sql
@@ -0,0 +1,188 @@
+
+load data local infile 'H:/RAIS/Dados/2007/AC2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/AL2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/AM2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/AP2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/BA2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/CE2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/DF2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/ES2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/GO2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/MA2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/MG2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/MS2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/MT2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/PA2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/PB2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/PE2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/PI2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/PR2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/RJ2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/RN2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/RO2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/RR2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/RS2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/SC2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/SE2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/SP2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2007/TO2007.txt'
+into table RAIS_2007
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
