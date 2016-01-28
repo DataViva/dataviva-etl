ffa1b63e4673a84aca4377ab74d299d168fb4809
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-01-28 11:44:47 -0200

    Carregando RAIS 2008

---
+++ rais/extract/load/load_RAIS_2008.sql
@@ -0,0 +1,42 @@
+load data local infile 'H:/RAIS/Dados/2008/norte08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/nordeste08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/centrooeste08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/sudeste08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/sul08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/sp08.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2008/sp08 1.txt'
+into table RAIS_2008
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
