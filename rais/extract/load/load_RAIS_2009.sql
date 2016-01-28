8c1396720fbe3a5036be141bc9be5622a40450e2
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-01-28 14:33:10 -0200

    Carregando RAIS 2009

---
+++ rais/extract/load/load_RAIS_2009.sql
@@ -0,0 +1,41 @@
+load data local infile 'H:/RAIS/Dados/2009/norte09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/nordeste09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/centrooeste09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/sudeste09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/sul09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/sp09.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2009/sp09 1.txt'
+into table RAIS_2009
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
