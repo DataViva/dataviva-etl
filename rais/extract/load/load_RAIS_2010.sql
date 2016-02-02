4ea0d008c4c88a524175a904fa566a4dbacede24
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-02-02 13:19:29 -0200

    Carregando RAIS 2010

---
+++ rais/extract/load/load_RAIS_2010.sql
@@ -0,0 +1,19 @@
+
+load data local infile 'H:/RAIS/Dados/2010/RAISCOMPL_NO_NE_CO_SU_3112.txt'
+into table RAIS_2010
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2010/RAISCOMPL_SE_3112.txt'
+into table RAIS_2010
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile 'H:/RAIS/Dados/2010/RAISCOMPLINATIVOS.txt'
+into table RAIS_2010
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
