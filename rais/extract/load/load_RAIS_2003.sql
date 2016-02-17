865ea4bd49fbaa2ead5ec920c94472facb19fd19
Melissa Lorena Araújo Pinho <mpinho@fapemig.br>
2016-02-17 11:40:42 -0200

    Atualizando etl RAIS 2003

--- rais/extract/load/load_RAIS_2003.sql
+++ rais/extract/load/load_RAIS_2003.sql
@@ -1,12 +1,21 @@
 load data local infile 'H:/RAIS/Dados/2003/RAISCOMPL_SE_31122003.txt'
-into table RAIS2003
+into table RAIS_2003
+character set 'latin1'
 fields terminated by '|'
 lines terminated by '\n'
 ignore 1 lines;
 
 
 load data local infile 'H:/RAIS/Dados/2003/RAISCOMPL_NO_NE_CO_SU_31122003.txt'
-into table RAIS2003
+into table RAIS_2003
+character set 'latin1'
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile 'H:/RAIS/Dados/2003/RAISCOMPLINATIVOS2003.txt'
+into table RAIS_2003
+character set 'latin1'
 fields terminated by '|'
 lines terminated by '\n'
 ignore 1 lines;
