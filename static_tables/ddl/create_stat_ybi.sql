f9c5a344d425c4b5dca834d4d32faa085aa3b59e
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-04 18:15:28 -0300

    Add create table stat_ybi.

---
+++ static_tables/ddl/create_stat_ybi.sql
@@ -0,0 +1,6 @@
+create table stat_ybi_test(  -- tem ano, localidade (brasil bra_id = 0) e as industria,
+    year varchar(5),
+    bra_id varchar(9),
+    cnae_id varchar(6),
+    value varchar(50)
+);
