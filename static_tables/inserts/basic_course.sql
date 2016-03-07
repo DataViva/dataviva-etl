9225867c075a70d88568d715526b2a62ef6ad754
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-07 18:53:59 -0300

    Refactoring select to show null when length of bra_id = 9.

--- static_tables/inserts/basic_course.sql
+++ static_tables/inserts/basic_course.sql
@@ -25,7 +25,7 @@ SELECT year, bra_id as Brasil, course_sc_id as Curso,
         age as Média_Idade,
         (select name_pt from dataviva.sc_ybsc a JOIN dataviva.attrs_school b ON a.school_id = b.id where year = '2014' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id = dataviva.sc_ybc.bra_id order by enrolled desc limit 1) as Escola,
         (select enrolled from dataviva.sc_ybsc where year = '2014' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id = dataviva.sc_ybc.bra_id order by enrolled desc limit 1) as Matrículas_Escola,
-        (select bra_id from dataviva.sc_ybc a JOIN dataviva.attrs_bra b ON a.bra_id = b.id where year = '2014' and bra_id_len = '9' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id like concat(dataviva.sc_ybc.bra_id, '%') order by enrolled desc limit 1) as Top_Municípios_Matrículas,
-        (select enrolled from dataviva.sc_ybc a JOIN dataviva.attrs_bra b ON a.bra_id = b.id where year = '2014' and bra_id_len = '9' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id like concat(dataviva.sc_ybc.bra_id, '%') order by enrolled desc limit 1) as Matrículas_Municicípio
+        CASE WHEN dataviva.sc_ybc.bra_id_len = 9 THEN null ELSE(select bra_id from dataviva.sc_ybc a JOIN dataviva.attrs_bra b ON a.bra_id = b.id where year = '2014' and bra_id_len = '9' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id like concat(dataviva.sc_ybc.bra_id, '%') order by enrolled desc limit 1) END as Top_Municípios_Matrículas,
+        CASE WHEN dataviva.sc_ybc.bra_id_len = 9 THEN null ELSE(select enrolled from dataviva.sc_ybc a JOIN dataviva.attrs_bra b ON a.bra_id = b.id where year = '2014' and bra_id_len = '9' and course_sc_id = dataviva.sc_ybc.course_sc_id and bra_id like concat(dataviva.sc_ybc.bra_id, '%') order by enrolled desc limit 1) END as Matrículas_Municicípio
         FROM dataviva.sc_ybc
         where year = '2014';
