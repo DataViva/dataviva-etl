92e200b3329f7d83b657514d7da0ee354cfe3713
Pedro Goecking <pedrogoecking93@gmail.com>
2016-03-08 18:09:16 -0300

    Adding insert for stat_ybu.

--- static_tables/ddl/create_tables.sql
+++ static_tables/ddl/create_tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE `dataviva_client`.`stat_ybsc` (
   `year` YEAR NOT NULL COMMENT '',
   `bra_id` VARCHAR(11) NULL COMMENT '',
   `course_sc_id` VARCHAR(9) NULL COMMENT '',
-  `enrollemnts` INT(10) NULL COMMENT '',
+  `enrollments` INT(10) NULL COMMENT '',
   `classes` INT(10) NULL COMMENT '',
   `total_school` INT(9) NULL COMMENT '',
   `average_class_size` DECIMAL(8,3) NULL COMMENT '',
@@ -28,7 +28,7 @@ CREATE TABLE `dataviva_client`.`stat_ybhedu` (
   `year` YEAR NOT NULL COMMENT '',
   `bra_id` VARCHAR(11) NULL COMMENT '',
   `course_hedu_id` VARCHAR(9) NULL COMMENT '',
-  `enrollemnts` INT(11) NULL COMMENT '',
+  `enrollments` INT(11) NULL COMMENT '',
   `entrants` INT(11) NULL COMMENT '',
   `graduates` INT(11) NULL COMMENT '',
   `top_university_enrollments` VARCHAR(8) NULL COMMENT '',
@@ -43,3 +43,17 @@ CREATE TABLE `dataviva_client`.`stat_ybhedu` (
   `university_graduates` INT(10) NULL COMMENT '',
   `top_municipality_graduates` VARCHAR(11) NULL COMMENT '',
   `municipality_graduates` INT(10) NULL COMMENT '');
+
+CREATE TABLE `dataviva_client`.`stat_ybu` (
+  `year` YEAR NOT NULL COMMENT '',
+  `bra_id` VARCHAR(11) NULL COMMENT '',
+  `university_id` VARCHAR(9) NULL COMMENT '',
+  `enrollments` INT(10) NULL COMMENT '',
+  `entrants` INT(10) NULL COMMENT '',
+  `graduates` INT(10) NULL COMMENT '',
+  `top_course_enrollments` VARCHAR(9) NULL COMMENT '',
+  `course_enrollments` INT(10) NULL COMMENT '',
+  `top_course_entrants` VARCHAR(9) NULL COMMENT '',
+  `course_entrants` INT(10) NULL COMMENT '',
+  `top_course_graduates` VARCHAR(9) NULL COMMENT '',
+  `course_graduates` INT(10) NULL COMMENT '');
