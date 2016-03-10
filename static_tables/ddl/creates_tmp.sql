5f37f12ba4a2b3227ed42809cd52d6a324f9d1b6
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-03-10 15:46:16 -0300

    Add create stat_yb table.

--- static_tables/ddl/creates_tmp.sql
+++ static_tables/ddl/creates_tmp.sql
@@ -42,25 +42,73 @@ create table stat_ybo(
 );
 
 
--- products 
+-- locations 
 drop table stat_yb;
 create table stat_ybp(
     year varchar(4),
     bra_id varchar(9),
-    trade_balance float,
-    export_val float,
-    weight_value_export float,
-    import_val float,
-    weight_value_import float,
-    pci float,
-    rca float,
-    distance float,
-    opp_gain float,
-    occupation_highest_monthly_wage float,
-    occupation_highest_monthly_wage_id varchar(10),
-    top_municipality_employment int ,
-    top_municipality_employment_id varchar(10) ,
-    top_municipality_monthly_wage float,
-    top_municipality_monthly_wage_id varchar(10)
+    gdp varchar(255),
+    life_exp varchar(255),
+    pop varchar(255),
+    gdp_pc varchar(255),
+    hdi varchar(255),
+    economic_complexity_index varchar(255),
+    top_product_export varchar(255),
+    top_product_export_id varchar(10),
+    top_product_import varchar(255),
+    top_product_import_id varchar(10),
+    export_val varchar(255),
+    import_val varchar(255),
+    top_export_destiny varchar(255),
+    top_export_destiny_id varchar(10),
+    top_import_origin varchar(255),
+    top_import_origin_id varchar(10),
+    top_industry  varchar(255),
+    top_industry_id  varchar(10),
+    top_occupation_jobs  varchar(255),
+    top_occupation_jobs_id  varchar(10),
+    average_wage  varchar(255),
+    wage  varchar(255),
+    employers varchar(255),
+    product_less_distance  varchar(255),
+    product_less_distance_id  varchar(10),
+    industry_less_distance  varchar(255),
+    industry_less_distance_id  varchar(10),
+    product_highest_opp_gain  varchar(255),
+    industry_opp_gain  varchar(255),
+    industry_opp_gain_id  varchar(10),
+    top_university_enrollments  varchar(255),
+    top_university_enrollments_id  varchar(10),
+    top_course_enrollments  varchar(255),
+    top_course_enrollments_id  varchar(10),
+    top_school_enrollment  varchar(255),
+    top_school_enrollment_id  varchar(10)
     
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
