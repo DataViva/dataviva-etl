37f47b2cb1dc18068c60a3ddbb88a23f31ae1aaa
Yasmine Fernanda Ferreira Cunha <ycunha@fapemig.br>
2016-09-28 11:46:14 -0300

    adcionando select_step1 equipamentos

---
+++ datasus/cnes/equipamentos/select/SELECT_EQUI_2008.sql
@@ -0,0 +1,8 @@
+## Select EQUI_2008	
+
+use cnes_equi;
+
+create table EQUI_2008_STEP1
+select  cnes, codufmun, regsaude, micr_reg, pf_pj, cpf_cnpj, niv_dep, cnpj_man, esfera_a, retencao, tp_unid, niv_hier, tipequip, codequip, qt_exist, qt_uso, ind_sus, ind_nsus, competen  
+from equi_2008;
+
