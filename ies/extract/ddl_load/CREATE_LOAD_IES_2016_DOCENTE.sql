7e5978a0bd8763438b5f27b81354335455be949f
Italo Daldegan <italo.oliveira@indi.mg.gov.br>
2018-02-05 17:36:40 -0200

    Scripts used to load and transform data for IES 2016.

---
+++ ies/extract/ddl_load/CREATE_LOAD_IES_2016_DOCENTE.sql
@@ -0,0 +1,69 @@
+use dataviva_raw;
+
+drop table if exists IES_2016_DOCENTE;
+create table IES_2016_DOCENTE(
+CO_IES varchar(8),
+NO_IES varchar(200),
+CO_CATEGORIA_ADMINISTRATIVA varchar(8),
+DS_CATEGORIA_ADMINISTRATIVA varchar(100),
+CO_ORGANIZACAO_ACADEMICA varchar(8),
+DS_ORGANIZACAO_ACADEMICA varchar(100),
+IN_CAPITAL_IES varchar(8),
+CO_DOCENTE_IES varchar(8),
+CO_DOCENTE varchar(20),
+CO_SITUACAO_DOCENTE varchar(8),
+DS_SITUACAO_DOCENTE varchar(50),
+CO_ESCOLARIDADE_DOCENTE varchar(1),
+DS_ESCOLARIDADE_DOCENTE varchar(14),
+CO_REGIME_TRABALHO varchar(8),
+DS_REGIME_TRABALHO varchar(38),
+IN_SEXO_DOCENTE varchar(8),
+DS_SEXO_DOCENTE varchar(9),
+NU_ANO_DOCENTE_NASC varchar(8),
+NU_MES_DOCENTE_NASC varchar(8),
+NU_DIA_DOCENTE_NASC varchar(8),
+NU_IDADE_DOCENTE varchar(8),
+CO_COR_RACA_DOCENTE varchar(8),
+DS_COR_RACA_DOCENTE varchar(34),
+CO_PAIS_DOCENTE varchar(8),
+CO_NACIONALIDADE_DOCENTE varchar(8),
+DS_NACIONALIDADE_DOCENTE varchar(48),
+CO_UF_NASCIMENTO varchar(8),
+CO_MUNICIPIO_NASCIMENTO varchar(8),
+IN_DOCENTE_DEFICIENCIA varchar(8),
+IN_DEF_CEGUEIRA varchar(8),
+IN_DEF_BAIXA_VISAO varchar(8),
+IN_DEF_SURDEZ varchar(8),
+IN_DEF_AUDITIVA varchar(8),
+IN_DEF_FISICA varchar(8),
+IN_DEF_SURDOCEGUEIRA varchar(8),
+IN_DEF_MULTIPLA varchar(8),
+IN_DEF_INTELECTUAL varchar(8),
+IN_ATU_EAD varchar(8),
+IN_ATU_EXTENSAO varchar(8),
+IN_ATU_GESTAO varchar(8),
+IN_ATU_GRAD_PRESENCIAL varchar(8),
+IN_ATU_POS_EAD varchar(8),
+IN_ATU_POS_PRESENCIAL varchar(8),
+IN_ATU_SEQUENCIAL varchar(8),
+IN_ATU_PESQUISA varchar(8),
+IN_BOLSA_PESQUISA varchar(8),
+IN_SUBSTITUTO varchar(8),
+IN_EXERCICIO_DT_REF varchar(8),
+IN_VISITANTE varchar(8),
+IN_VISITANTE_IFES_VINCULO varchar(8));
+
+
+load data local infile 'H:/HEDU/Dados/2016/DADOS/DM_DOCENTE.csv'
+into table IES_2016_DOCENTE
+character set 'latin1'
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+select * from IES_2016_DOCENTE;
+
+select count(*) from IES_2016_DOCENTE;
+
+
+
---
+++ ies/extract/ddl_load/CREATE_LOAD_IES_2016_IES.sql
@@ -0,0 +1,69 @@
+use dataviva_raw;
+
+drop table if exists IES_2016_IES;
+create table IES_2016_IES(
+CO_IES varchar(8),
+NO_IES varchar(200),
+SGL_IES varchar(20),
+CO_MANTENEDORA varchar(8),
+NO_MANTENEDORA varchar(200),
+CO_CATEGORIA_ADMINISTRATIVA varchar(8),
+DS_CATEGORIA_ADMINISTRATIVA varchar(100),
+CO_ORGANIZACAO_ACADEMICA varchar(8),
+DS_ORGANIZACAO_ACADEMICA varchar(100),
+CO_MUNICIPIO_IES varchar(8),
+NO_MUNICIPIO_IES varchar(150),
+CO_UF_IES varchar(8),
+SGL_UF_IES varchar(2),
+NO_REGIAO_IES varchar(30),
+IN_CAPITAL_IES varchar(8),
+QT_TEC_TOTAL varchar(8),
+QT_TEC_FUND_INCOMP_FEM varchar(8),
+QT_TEC_FUND_INCOMP_MASC varchar(8),
+QT_TEC_FUND_COMP_FEM varchar(8),
+QT_TEC_FUND_COMP_MASC varchar(8),
+QT_TEC_MEDIO_FEM varchar(8),
+QT_TEC_MEDIO_MASC varchar(8),
+QT_TEC_SUPERIOR_FEM varchar(8),
+QT_TEC_SUPERIOR_MASC varchar(8),
+QT_TEC_ESPECIALIZACAO_FEM varchar(8),
+QT_TEC_ESPECIALIZACAO_MASC varchar(8),
+QT_TEC_MESTRADO_FEM varchar(8),
+QT_TEC_MESTRADO_MASC varchar(8),
+QT_TEC_DOUTORADO_FEM varchar(8),
+QT_TEC_DOUTORADO_MASC varchar(8),
+IN_ACESSO_PORTAL_CAPES varchar(8),
+IN_ACESSO_OUTRAS_BASES varchar(8),
+IN_REPOSITORIO_INSTITUCIONAL varchar(8),
+IN_BUSCA_INTEGRADA varchar(8),
+IN_SERVICO_INTERNET varchar(8),
+IN_PARTICIPA_REDE_SOCIAL varchar(8),
+IN_CATALOGO_ONLINE varchar(8),
+QT_PERIODICO_ELETRONICO varchar(20),
+QT_LIVRO_ELETRONICO varchar(20),
+IN_REFERENTE varchar(8),
+VL_RECEITA_PROPRIA varchar(20),
+VL_TRANSFERENCIA varchar(20),
+VL_OUTRA_RECEITA varchar(20),
+VL_DES_PESSOAL_REM_DOCENTE varchar(20),
+VL_DES_PESSOAL_REM_TECNICO varchar(20),
+VL_DES_PESSOAL_ENCARGO varchar(20),
+VL_DES_CUSTEIO varchar(20),
+VL_DES_INVESTIMENTO varchar(20),
+VL_DES_PESQUISA varchar(20),
+VL_DES_OUTRAS varchar(20));
+
+
+load data local infile 'H:/HEDU/Dados/2016/DADOS/DM_IES.csv'
+into table IES_2016_IES
+character set 'latin1'
+fields terminated by '|'
+lines terminated by '\n'
+ignore 1 lines;
+
+select count(*) from IES_2016_IES;
+select * from IES_2016_IES;
+
+
+
+
