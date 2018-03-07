46b7e50ac28d03c180aa165aac56f4a19dd1bfe8
Italo Daldegan <italo.oliveira@indi.mg.gov.br>
2018-03-07 08:54:12 -0300

    Prepare IES 2016 data to be loaded into Redshift.

--- ies/extract/select/select_IES_ALUNO_2016.sql
+++ ies/extract/select/select_IES_ALUNO_2016.sql
@@ -3,16 +3,16 @@ use dataviva_raw;
 
 -- Filtro e seleção da tabela Aluno
 
-create table IES_2016_ALUNO_STEP1 
-select CO_IES, CO_CATEGORIA_ADMINISTRATIVA, CO_ORGANIZACAO_ACADEMICA, CO_CURSO, 
-CO_ALUNO, CO_GRAU_ACADEMICO, CO_MODALIDADE_ENSINO, IN_CONCLUINTE, IN_INGRESSO_TOTAL as IN_INGRESSO, 
+create table IES_2016_ALUNO_STEP1
+select CO_IES, CO_CATEGORIA_ADMINISTRATIVA, CO_ORGANIZACAO_ACADEMICA, CO_CURSO,
+CO_ALUNO, CO_GRAU_ACADEMICO, CO_MODALIDADE_ENSINO, IN_CONCLUINTE, IN_INGRESSO_TOTAL as IN_INGRESSO,
 DT_INGRESSO_CURSO, IN_SEXO_ALUNO, NU_IDADE_ALUNO, CO_COR_RACA_ALUNO, CO_UF_NASCIMENTO,
 CO_MUNICIPIO_NASCIMENTO, CO_TURNO_ALUNO
 from IES_2016_ALUNO
 where IN_MATRICULA=1 and CO_NIVEL_ACADEMICO=1;
 
 -- Seleção da tabela Curso
-create table IES_2016_CURSO_STEP1 
+create table IES_2016_CURSO_STEP1
 select CO_MUNICIPIO_CURSO as CO_MUNICIPIO, CO_CURSO, CO_OCDE, CO_LOCAL_OFERTA_CURSO as CO_LOCAL_OFERTA_IES
 from IES_2016_CURSO;
 
@@ -20,15 +20,16 @@ from IES_2016_CURSO;
 create table IES_2016_ALUNO_STEP2 select * from IES_2016_ALUNO_STEP1;
 
 alter table IES_2016_ALUNO_STEP2 add (CO_MUNICIPIO varchar(8), CO_OCDE varchar(20), CO_LOCAL_OFERTA_IES varchar(30));
-update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1 
+
+update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1
 on IES_2016_ALUNO_STEP2.CO_CURSO = IES_2016_CURSO_STEP1.CO_CURSO
 set IES_2016_ALUNO_STEP2.CO_MUNICIPIO = IES_2016_CURSO_STEP1.CO_MUNICIPIO;
 
-update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1 
+update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1
 on IES_2016_ALUNO_STEP2.CO_CURSO = IES_2016_CURSO_STEP1.CO_CURSO
 set IES_2016_ALUNO_STEP2.CO_OCDE = IES_2016_CURSO_STEP1.CO_OCDE;
 
-update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1 
+update IES_2016_ALUNO_STEP2 left join IES_2016_CURSO_STEP1
 on IES_2016_ALUNO_STEP2.CO_CURSO = IES_2016_CURSO_STEP1.CO_CURSO
 set IES_2016_ALUNO_STEP2.CO_LOCAL_OFERTA_IES = IES_2016_CURSO_STEP1.CO_LOCAL_OFERTA_IES;
 
@@ -156,3 +157,45 @@ select CO_OCDE, count(*) from IES_2016_ALUNO_STEP3 group by CO_OCDE;
 select CO_LOCAL_OFERTA_IES, count(*) from IES_2016_ALUNO_STEP3 group by CO_LOCAL_OFERTA_IES;
 select ANO_INGRESSO, count(*) from IES_2016_ALUNO_STEP3 group by ANO_INGRESSO;
 select MES_INGRESSO, count(*) from IES_2016_ALUNO_STEP3 group by MES_INGRESSO;
+
+-- Preparando os dados para o RedShift
+CREATE TABLE dataviva_raw.IES_2016_ALUNO_STEP4 (
+year integer,
+region varchar(1),
+mesoregion varchar(4),
+microregion varchar(5),
+state varchar(2),
+municipality varchar(7),
+university varchar(5),
+university_campus varchar(7),
+funding_type varchar(1),
+school_type varchar(1),
+hedu_course_field varchar(6),
+hedu_course varchar(6),
+enrolled varchar(12),
+graduate varchar(1),
+entrant varchar(1),
+academic_degree varchar(2),
+distance_learning varchar(1),
+shift varchar(2),
+gender varchar(2),
+age integer,
+ethnicity varchar(2),
+state_of_birth varchar(2),
+municipality_of_birth varchar(7),
+admission_year varchar(4),
+admission_month varchar(2)
+);
+
+INSERT INTO IES_2016_ALUNO_STEP4 (
+	year, region, mesoregion, microregion, state, municipality, university, university_campus,
+	funding_type,school_type,hedu_course_field,hedu_course, enrolled, graduate, entrant,
+	academic_degree, distance_learning, shift, gender, age, ethnicity, state_of_birth,
+	municipality_of_birth, admission_year, admission_month)
+SELECT 2016, substring(CO_MUNICIPIO_IES, 1, 1), substring( CO_MUNICIPIO_IES, 1, 4 ),
+	substring( CO_MUNICIPIO_IES, 1, 5 ), substring( CO_MUNICIPIO_IES, 1, 2 ),
+	CO_MUNICIPIO_IES, CO_IES, CO_LOCAL_OFERTA_IES, CO_CATEGORIA_ADMINISTRATIVA,
+	CO_ORGANIZACAO_ACADEMICA, substring( CO_OCDE, 1, 2 ), CO_OCDE, CO_ALUNO, IN_CONCLUINTE, IN_INGRESSO,
+	CO_GRAU_ACADEMICO, CO_MODALIDADE_ENSINO, CO_TURNO_ALUNO, IN_SEXO_ALUNO, NU_IDADE_ALUNO,
+	CO_COR_RACA_ALUNO, CO_UF_NASCIMENTO, CO_MUNICIPIO_NASCIMENTO, ANO_INGRESSO, MES_INGRESSO
+FROM IES_2016_ALUNO_STEP3;
