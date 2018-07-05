fac731ee8edd26ce3d3c5dbd10d73ae5e9c129b4
leandrocollares <leco@uvic.ca>
2018-07-05 14:39:54 -0300

    Add 2016 RAIS scripts

---
+++ rais/extract/load/load_RAIS_2016.sql
@@ -0,0 +1,214 @@
+load data local infile '/home/ubuntu/data/AC2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/AL2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/AM2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/AP2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/BA2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/CE2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/DF2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/ES2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/GO2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/MA2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/MG2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/MS2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/MT2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/PA2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/PB2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/PE2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/PI2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/PR2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/RJ2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/RN2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/RO2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/RR2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/RS2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/SC2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/SE2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/SP2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/home/ubuntu/data/TO2016ID.txt'
+into table RAIS_2016
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
