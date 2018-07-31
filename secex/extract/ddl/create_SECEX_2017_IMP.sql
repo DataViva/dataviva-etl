f4f6f0488a18a34d80f0217304e85ad90334e23e
Marcelo <msbandrade@gmail.com>
2018-07-31 11:01:49 -0300

    Create SECEX import and export tables

---
+++ secex/extract/ddl/create_SECEX_2017_IMP.sql
@@ -0,0 +1,14 @@
+drop table if exists SECEX_2017_IMP;
+
+CREATE TABLE SECEX_2017_IMP(
+	CO_ANO integer,
+	CO_MES integer,
+	CO_SH4 varchar(4),
+	CO_PAIS varchar(3),			
+	CO_UF varchar(2),
+	CO_PORTO varchar(4),		
+	CO_MUN_GEO varchar(7),
+	KG_LIQUIDO decimal (20,2),
+	VL_FOB decimal (20,2)
+); 
+
