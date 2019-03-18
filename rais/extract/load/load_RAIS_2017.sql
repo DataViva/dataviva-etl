39c429a15972df8fc58d0c4e1c4afd1f3cd968cf
[msbandrade] <italodaldegan@hotmail.com>
2019-03-18 16:17:02 -0300

    Add script to insert data into RAIS_2017 table

---
+++ rais/extract/load/load_RAIS_2017.sql
@@ -0,0 +1,216 @@
+use dataviva_raw;
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/AC2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/AL2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/AM2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/AP2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/BA2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/CE2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/DF2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/ES2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/GO2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/MA2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/MG2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/MS2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/MT2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/PA2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/PB2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/PE2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/PI2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/PR2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/RJ2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/RN2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/RO2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/RR2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/RS2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/SC2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/SE2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/SP2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+load data local infile '/media/dev4/Banco de Dados/Bancos de Dados/MTE/ETL_2017/TO2017ID.txt'
+into table RAIS_2017
+character set 'latin1'
+fields terminated by ';'
+lines terminated by '\n'
+ignore 1 lines;
