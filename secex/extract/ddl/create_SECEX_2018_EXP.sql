7904a3f173fd07fb601f64a006f53d82fbabfb07
Marcelo Andrade <msbandrade@gmail.com>
2019-03-18 10:59:47 -0300

    Add files via upload
    Create SECEX export table

---
+++ secex/extract/ddl/create_SECEX_2018_EXP.sql
@@ -0,0 +1,14 @@
+use secex;
+
+drop table if exists SECEX_2018_EXP;
+
+CREATE TABLE SECEX_2018_EXP(
+	CO_ANO integer,
+	CO_MES integer,
+	CO_SH4 varchar(4),
+	CO_PAIS varchar(3),			
+	CO_UF varchar(2),
+	CO_MUN_GEO varchar(7),
+	KG_LIQUIDO decimal (20,2),
+	VL_FOB decimal (20,2)
+); 
