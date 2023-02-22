531cc10e195ee6ff0ee66869697aaf1b5d0ae443
Rodrigo Dias <rodrigo1406@gmail.com>
2023-02-22 16:26:24 -0300

    Add files via upload

---
+++ secex/extract/ddl/create_SECEX_2020_EXP.sql
@@ -0,0 +1,13 @@
+drop table if exists SECEX_2020_EXP;
+
+CREATE TABLE SECEX_2020_EXP(
+	CO_ANO integer,
+	CO_MES integer,
+	CO_SH4 varchar(4),
+	CO_PAIS varchar(3),			
+	CO_UF varchar(2),
+	CO_PORTO varchar(4),		
+	CO_MUN_GEO varchar(7),
+	KG_LIQUIDO decimal (20,2),
+	VL_FOB decimal (20,2)
+); 
