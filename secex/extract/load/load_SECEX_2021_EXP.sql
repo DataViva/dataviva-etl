71a0f6776c931aa78970d6d469df026b38e71117
Rodrigo Dias <rodrigo1406@gmail.com>
2023-02-24 09:16:06 -0300

    Add files via upload

---
+++ secex/extract/load/load_SECEX_2021_EXP.sql
@@ -0,0 +1,8 @@
+load data local infile '/home/rodrigo/Documents/Econ/DataViva/SECEX/exports/EXP_2021_MUN.csv'
+into table SECEX_2021_EXP
+character set 'latin1'
+fields terminated by ';'
+enclosed by '"'
+lines terminated by '\r\n'
+ignore 1 lines
+(CO_ANO,CO_MES,CO_SH4,CO_PAIS,CO_UF,CO_MUN_GEO,KG_LIQUIDO,VL_FOB);
---
+++ secex/extract/load/load_SECEX_2022_IMP.sql
@@ -0,0 +1,8 @@
+load data local infile '/home/rodrigo/Documents/Econ/DataViva/SECEX/imports/IMP_2022_MUN.csv'
+into table SECEX_2022_IMP
+character set 'latin1'
+fields terminated by ';'
+enclosed by '"'
+lines terminated by '\r\n'
+ignore 1 lines
+(CO_ANO,CO_MES,CO_SH4,CO_PAIS,CO_UF,CO_MUN_GEO,KG_LIQUIDO,VL_FOB);
