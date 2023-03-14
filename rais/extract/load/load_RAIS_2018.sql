5bea0f3a654217a691d5342f28e5e8bac3c74d88
Felipe Amaral <fsaeconomia@gmail.com>
2023-03-14 14:52:25 -0300

    Add index to load RAIS 2018

--- rais/extract/load/load_RAIS_2018.sql
+++ rais/extract/load/load_RAIS_2018.sql
@@ -41,3 +41,11 @@ character set 'latin1'
 fields terminated by ';'
 lines terminated by '\n'
 ignore 1 lines;
+
+-- Os códigos a seguir são para incluir um índice para as principais variáveis e otimizar o processo de select no futuro.
+
+CREATE INDEX index_municipio ON dataviva_raw.RAIS_2018 (MUNICIPIO);
+CREATE INDEX index_cnae ON dataviva_raw.RAIS_2018 (CLAS_CNAE_20);
+CREATE INDEX index_cbo ON dataviva_raw.RAIS_2018 (OCUP_2002);
+CREATE INDEX index_emp3112 ON dataviva_raw.RAIS_2018 (EMP_31_12);
+CREATE INDEX index_mun_cnae_cbo_emp ON dataviva_raw.RAIS_2018 (MUNICIPIO, CLAS_CNAE_20, OCUP_2002, EMP_31_12);
